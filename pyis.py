44238ffddc1f0e3cb9569ec3ee8a013b8e7bb489
rynnxiao <68548069+rynnxiao@users.noreply.github.com>
2022-02-10 17:29:06 -0500

    Update pyis.py

--- pyis.py
+++ pyis.py
@@ -18,6 +18,7 @@ class Fee:
 
 @attr.define
 class Transaction:
+    name = attr.ib(type=str)
     share_num = attr.ib(type=float)
     execute_num = attr.ib(type=float)
     delay_price = attr.ib(type=float)
@@ -48,6 +49,7 @@ class Cost:
 
 class TCAEngine:
     def __init__(self):
+        self.transactions = []
         self.cost_results = []
 
     def tca_run(self, transaction_obj: Transaction):
@@ -55,49 +57,55 @@ class TCAEngine:
 
     def reset_result(self):
         self.cost_results = []
+        self.transactions = []
 
 
 class ImplementationShortfallEngine(TCAEngine):
-    def complete_execution(self, transaction_obj):
-        executed_cost = transaction_obj.share_num * \
-            (transaction_obj.execute_avg_price -
-             transaction_obj.determined_price) + transaction_obj.fee.total_fee()
+    def complete_execution(self, transaction):
+        executed_cost = transaction.share_num * \
+            (transaction.execute_avg_price -
+             transaction.determined_price) + transaction.fee.total_fee()
         self.cost_results.append(
-            Cost("Executation Cost", 0, executed_cost,
-                 0, transaction_obj.fee.total_fee())
+            Cost(f"Executation Cost {transaction.name}", 0, executed_cost,
+                 0, transaction.fee.total_fee())
         )
 
-    def opportunity_cost(self, transaction_obj):
-        executed_cost = transaction_obj.execute_num * \
-            (transaction_obj.execute_avg_price - transaction_obj.determined_price)
-        opportunity_cost = (transaction_obj.share_num - transaction_obj.execute_num) * (
-            transaction_obj.end_price - transaction_obj.determined_price)
+    def opportunity_cost(self, transaction):
+        executed_cost = transaction.execute_num * \
+            (transaction.execute_avg_price - transaction.determined_price)
+        opportunity_cost = (transaction.share_num - transaction.execute_num) * (
+            transaction.end_price - transaction.determined_price)
 
         self.cost_results.append(
-            Cost("Opportunity Cost", 0, executed_cost,
-                 opportunity_cost, transaction_obj.fee.total_fee())
+            Cost(f"Opportunity Cost {transaction.name}", 0, executed_cost,
+                 opportunity_cost, transaction.fee.total_fee())
         )
 
-    def expanded_implementation_shortfall(self, transaction_obj):
-        delay_cost = transaction_obj.share_num * \
-            (transaction_obj.delay_price - transaction_obj.determined_price)
-        trading_cost = transaction_obj.execute_num * \
-            (transaction_obj.execute_avg_price - transaction_obj.delay_price)
-        opportunity_cost = (transaction_obj.share_num - transaction_obj.execute_num) * (
-            transaction_obj.end_price - transaction_obj.delay_price)
+    def expanded_implementation_shortfall(self, transaction):
+        delay_cost = transaction.share_num * \
+            (transaction.delay_price - transaction.determined_price)
+        trading_cost = transaction.execute_num * \
+            (transaction.execute_avg_price - transaction.delay_price)
+        opportunity_cost = (transaction.share_num - transaction.execute_num) * (
+            transaction.end_price - transaction.delay_price)
 
         self.cost_results.append(
-            Cost("Expanded IS Cost", delay_cost, trading_cost,
-                 opportunity_cost, transaction_obj.fee.total_fee())
+            Cost(f"Expanded IS Cost {transaction.name}", delay_cost, trading_cost,
+                 opportunity_cost, transaction.fee.total_fee())
         )
 
-    def tca_run(self, transaction_obj: Transaction):
-        self.reset_result()
-        if transaction_obj.is_fully_executed():
-            self.complete_execution(transaction_obj)
+    def tca_run(self, transaction: Transaction, reset=True):
+        if reset:
+            self.reset_result()
+        if transaction.is_fully_executed():
+            self.complete_execution(transaction)
         else:
-            self.opportunity_cost(transaction_obj)
-            self.expanded_implementation_shortfall(transaction_obj)
+            self.opportunity_cost(transaction)
+            self.expanded_implementation_shortfall(transaction)
+
+    def batch_tca_run(self, transactions: list):
+        for transaction in transactions:
+            self.tca_run(transaction, reset=False)
 
     def format_data(self):
         self.result_formated_data = [[getattr(item, name) for name in Cost.__dict__ if not name.startswith("_")]
@@ -106,7 +114,6 @@ class ImplementationShortfallEngine(TCAEngine):
     def pprint(self):
         header = sorted([name for name in Cost.__dict__ if not name.startswith("_")])
         self.format_data()
-        print(self.result_formated_data)
         print(
             tabulate(
                 self.result_formated_data,
@@ -119,7 +126,8 @@ class ImplementationShortfallEngine(TCAEngine):
 if __name__ == "__main__":
     engine = ImplementationShortfallEngine()
     print("Transaction 1:")
-    trans = Transaction(
+    trans1 = Transaction(
+        name="tran1",
         share_num=5000, 
         execute_num=4000, 
         delay_price=10.25, 
@@ -128,17 +136,22 @@ if __name__ == "__main__":
         end_price=11, 
         fee=Fee(80)
         )
-    engine.tca_run(trans)
+    engine.tca_run(trans1)
     engine.pprint()
     print("\nTransaction 2:")
-    trans = Transaction(
+    trans2 = Transaction(
+        name="tran2",
         share_num=5000, 
         execute_num=5000, 
         delay_price=10.25, 
         execute_avg_price=10.5, 
         determined_price=10, 
         end_price=11, 
-        fee=Fee(80)
+        fee=Fee(100)
         )
-    engine.tca_run(trans)
+    engine.tca_run(trans2)
+    engine.pprint()
+
+    print("\nTransaction batch:")
+    engine.batch_tca_run([trans2, trans2, trans1, trans1])
     engine.pprint()
